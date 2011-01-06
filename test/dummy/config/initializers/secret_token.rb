47791b32dc8c62b82a3f15169ae2aa152c546f2a
Łukasz Sągol <lukasz@sagol.pl>
2011-01-07 00:45:40 +0100

    Added dummy Rails app for testing

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '6e142ca582604797825d6868bbc465ad527a894e6413e144f4498435a43beffb5e4f3c4303d0f06905e9c281c4574ab0d07cfd7f1aa29ff8397c45638361ee14'
--- test/helper.rb
+++ test/helper.rb
@@ -1,18 +1,10 @@
-require 'rubygems'
-require 'bundler'
-begin
-  Bundler.setup(:default, :development)
-rescue Bundler::BundlerError => e
-  $stderr.puts e.message
-  $stderr.puts "Run `bundle install` to install missing gems"
-  exit e.status_code
-end
 require 'test/unit'
 require 'shoulda'
 
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'monggler'
+ENV["RAILS_ENV"] = "test"
 
-class Test::Unit::TestCase
-end
+require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+require "rails/test_help"
+
+Rails.backtrace_cleaner.remove_silencers!
+ActiveRecord::Migrator.migrate File.expand_path("../dummy/db/migrate/", __FILE__)
