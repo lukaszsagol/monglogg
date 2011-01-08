594b033985c1dd9529ba5ada3528e168f5c9acc2
Łukasz Sągol <lukasz@sagol.pl>
2011-01-08 18:43:42 +0100

    Little hack for getting servers IP addr

--- lib/monggler/helper.rb
+++ lib/monggler/helper.rb
@@ -2,7 +2,11 @@ module Monggler
   class Helper
     class << self
       def current_app_name
-        Rails.application.class.name.split('::').first.underscore
+        @app_name ||= Rails.application.class.name.split('::').first.underscore
+      end
+
+      def current_ip
+        @current_ip ||= UDPSocket.open {|s| s.connect('64.233.187.99', 1); s.addr.last }
       end
     end
   end
